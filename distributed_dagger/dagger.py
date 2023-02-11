168b45db68617c61b12ae22527c869fac630def1
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2023-02-11 02:05:00 -0500

    working dagger

--- distributed_dagger/dagger.py
+++ distributed_dagger/dagger.py
@@ -6,8 +6,10 @@ import torch
 from torch import optim
 from torch.utils.tensorboard import SummaryWriter
 
+from distributed_dagger.distributed_dagger.ddp.ddp import DecentralizedDistributedMixin
 
-class DaggerBase:
+
+class DAggerBase:
     def __init__(
         self,
         envs: Any,
@@ -16,7 +18,6 @@ class DaggerBase:
         batch_length: int,
         total_num_steps: int,
         device: torch.device,
-        lr: float = 3e-4,
         updates_per_ckpt: int = 100,
         num_updates: int = -1,
         teacher_forcing: bool = False,
@@ -29,63 +30,72 @@ class DaggerBase:
         self.batch_length = batch_length
         self.total_num_steps = total_num_steps
         self.device = device
-        self.lr = lr
         self.updates_per_ckpt = updates_per_ckpt
         self.num_updates = num_updates
         self.teacher_forcing = teacher_forcing
         self.tb_dir = tb_dir
         self.checkpoint_folder = checkpoint_folder
 
-        self.writer = None
         self.ckpt_id = 0
-        self.num_updates_done = 0
         self.num_steps_done = 0
         self.num_train_iter = 0
+        self.num_updates_done = 0
+        self.writer = None
 
     @property
     def num_envs(self):
         return self.envs.num_envs
 
-    def get_model_params(self):
-        return self.actor_critic.parameters()
+    def get_teacher_actions(self, observations):
+        """Applied to output of self.transform_observations(), sent to
+        self.action_loss() """
+        raise NotImplementedError
+
+    def get_student_actions(self, observations):
+        """Applied to output of self.transform_observations(), sent to
+        self.envs.step() """
+        raise NotImplementedError
+
+    def update_tensorboard(self, observations, rewards, dones, infos, action_loss):
+        raise NotImplementedError
 
     def setup_tensorboard(self):
         if self.tb_dir != "":
-            print(f"Creating tensorboard at {self.tb_dir}...")
             os.makedirs(self.tb_dir, exist_ok=True)
             self.writer = SummaryWriter(self.tb_dir)
         else:
             self.writer = None
 
     def transform_observations(self, observations):
+        """Applied to the observations output of self.envs.step()"""
         return observations
 
     def sift_env_outputs(self, outputs):
+        """Applied to all outputs of self.envs.step()"""
         observations, rewards, dones, infos = outputs
-        observations = self.transform_observations(observations)
         return observations, rewards, dones, infos
 
     def step_envs(self, actions):
         outputs = self.envs.step(actions)
         observations, rewards, dones, infos = self.sift_env_outputs(outputs)
+        observations = self.transform_observations(observations)
         return observations, rewards, dones, infos
 
-    def get_teacher_actions(self, observations):
-        raise NotImplementedError
+    def _compute_log_probs(self, actions):
+        # Needs to be its own method for DDP inheritance
+        return self.actor_critic.compute_log_probs(actions)
 
-    def action_loss(self, teacher_actions, student_actions):
-        raise NotImplementedError
+    def action_loss(self, teacher_actions):
+        log_probs = self._compute_log_probs(teacher_actions)
+        return -log_probs.mean()
 
-    def update_tensorboard(self, observations, rewards, dones, infos, action_loss):
-        raise NotImplementedError
-
-    def get_checkpoint(self, iteration, batch_num):
+    def generate_checkpoint(self):
         checkpoint = {
             "state_dict": self.actor_critic.state_dict(),
-            "iteration": iteration,
-            "batch": batch_num,
+            "iteration": self.num_train_iter,
+            "batch": self.num_updates_done,
         }
-        filename = f"ckpt.{self.ckpt_id}_{batch_num}.pth"
+        filename = f"ckpt.{self.ckpt_id}_{self.num_updates_done}.pth"
         ckpt_path = osp.join(self.checkpoint_folder, filename)
         self.ckpt_id += 1
         return checkpoint, ckpt_path
@@ -96,18 +106,25 @@ class DaggerBase:
         else:
             return self.num_steps_done / self.total_num_steps
 
+    def should_save_now(self):
+        return (
+            self.num_updates_done % self.updates_per_ckpt == 0
+            and self.checkpoint_folder != ""
+        )
+
     def train(self):
         self.setup_tensorboard()
         observations = self.envs.reset()
         observations = self.transform_observations(observations)
 
-        action_loss = torch.tensor(0, device=self.device)
+        action_loss = None
         while self.percent_done() < 1.0:
             teacher_actions = self.get_teacher_actions(observations)
-            student_actions = self.actor_critic(observations)
+            student_actions = self.get_student_actions(observations)
             actions = teacher_actions if self.teacher_forcing else student_actions
 
-            action_loss += self.action_loss(actions, teacher_actions)
+            curr_loss = self.action_loss(teacher_actions)
+            action_loss = curr_loss if action_loss is None else action_loss + curr_loss
 
             observations, rewards, dones, infos = self.step_envs(actions)
 
@@ -115,23 +132,21 @@ class DaggerBase:
 
             if self.num_train_iter % self.batch_length == 0:
                 self.optimizer.zero_grad()
-                action_loss = torch.mean(action_loss) / self.batch_length
+                action_loss = action_loss.mean() / self.batch_length
                 action_loss.backward()
                 self.optimizer.step()
 
                 self.num_updates_done += 1
-                action_loss = torch.tensor(0, device=self.device)
+                action_loss = None
 
-                if (
-                    self.num_updates_done % self.updates_per_ckpt == 0
-                    and self.checkpoint_folder != ""
-                ):
+                if self.should_save_now():
                     # Save checkpoint
-                    ckpt, ckpt_path = self.get_checkpoint(
-                        self.num_train_iter, self.num_updates_done
-                    )
+                    ckpt, ckpt_path = self.generate_checkpoint()
                     torch.save(ckpt, ckpt_path)
-                    print("Saved checkpoint:", ckpt_path)
 
             self.num_steps_done += self.num_envs
             self.num_train_iter += 1
+
+
+class DAggerDDP(DecentralizedDistributedMixin, DAggerBase):
+    pass
